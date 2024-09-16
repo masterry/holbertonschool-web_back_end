f0c908dd1f8a249c3ea008416afd7efb4b182d85
masterry <terry.al.khoury@gmail.com>
2024-09-16 11:46:05 +0300

    7 fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,54 +1,63 @@
 const express = require('express');
 const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
+
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+
+      all.numberStudents = `Number of students: ${lines.length}\n`;
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
+      }
+      return resolve(all);
+    });
+  });
+}
+
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
-  try {
-    const data = fs.readFileSync(process.argv[2], 'utf8');
-    const linesArray = data.split('\n');
-    let numberOfStudents = 0;
-    const studentsByField = {};
-    linesArray.shift();
-    for (const line of linesArray) {
-      const splittedLine = line.split(',');
-      if (splittedLine.length === 4) {
-        numberOfStudents += 1;
-        if (splittedLine[3] in studentsByField) {
-          studentsByField[splittedLine[3]].push(splittedLine[0]);
-        } else {
-          studentsByField[splittedLine[3]] = [splittedLine[0]];
-        }
-      }
-    }
-    res.write(`Number of students: ${numberOfStudents}\n`);
-    let strStudents = '';
-    // eslint-disable-next-line guard-for-in
-    for (const field in studentsByField) {
-      let strList = '';
-      for (const student of studentsByField[field]) {
-        if (strList.length > 0) {
-          strList += ', ';
-        }
-        strList += student;
-      }
-      strStudents += `Number of students in ${field}: ${studentsByField[field].length}. List: ${strList}\n`;
-    }
-    res.write(strStudents.slice(0, -1));
-  } catch (err) {
-    res.write('Cannot load the database');
-  }
-  res.send();
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.write(data.numberStudents);
+      res.end(data.listStudents.join('\n'));
+    })
+    .catch((err) => {
+      res.end(err.message);
+    });
 });
-
-app.listen(port, () => {
-  console.log(`Express listening on port ${port}`);
-});
+app.listen(port);
 
 module.exports = app;
