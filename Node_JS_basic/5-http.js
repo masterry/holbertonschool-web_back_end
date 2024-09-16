b3de99195daa8058e74e0a28613880b38009a6d7
masterry <terry.al.khoury@gmail.com>
2024-09-16 11:43:07 +0300

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,53 +1,65 @@
 const http = require('http');
 const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
+
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+
+      all.numberStudents = `Number of students: ${lines.length}\n`;
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
+      }
+      return resolve(all);
+    });
+  });
+}
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-  }
+  if (req.url === '/') res.end('Hello Holberton School!');
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      const data = fs.readFileSync(process.argv[2], 'utf8');
-      const linesArray = data.split('\n');
-      let numberOfStudents = 0;
-      const studentsByField = {};
-      linesArray.shift();
-      for (const line of linesArray) {
-        const splittedLine = line.split(',');
-        if (splittedLine.length === 4) {
-          numberOfStudents += 1;
-          if (splittedLine[3] in studentsByField) {
-            studentsByField[splittedLine[3]].push(splittedLine[0]);
-          } else {
-            studentsByField[splittedLine[3]] = [splittedLine[0]];
-          }
-        }
-      }
-      res.write(`Number of students: ${numberOfStudents}\n`);
-      let strStudents = '';
-      // eslint-disable-next-line guard-for-in
-      for (const field in studentsByField) {
-        let strList = '';
-        for (const student of studentsByField[field]) {
-          if (strList.length > 0) {
-            strList += ', ';
-          }
-          strList += student;
-        }
-        strStudents += `Number of students in ${field}: ${studentsByField[field].length}. List: ${strList}\n`;
-      }
-      res.write(strStudents.slice(0, -1));
-    } catch (err) {
-      res.write('Cannot load the database');
-    }
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write(data.numberStudents);
+        res.write(data.listStudents.join('\n'));
+        res.end();
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
   }
-  res.end();
 });
 
 app.listen(port, hostname);
